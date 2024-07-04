001e048ee02ecafd810d8abdbd4cf02060b25cc2
NikhilVinay04 <130574166+NikhilVinay04@users.noreply.github.com>
2024-07-04 12:47:27 +0530

    Update JSON_Employee.java

--- src/org/emp/JSON_Employee.java
+++ src/org/emp/JSON_Employee.java
@@ -6,9 +6,12 @@ import com.softwaretree.jdx.JDX_JSONObject;
  * A shell (container) class defining a domain model object class for Employee objects
  * based on the class JSONObject.  This class needs to define just two constructors.
  * Most of the processing is handled by the superclass JDX_JSONObject.
- * <p>
- *
- *
+ * Description of the Employee type object : 
+ *    id-String primary key 
+ *    name- Name of the Employee
+ *    exempt- Boolean value to check if the Employee is exempt or not.
+ *    compensation- Salary of the Employee.
+ *    dob- long value to describe date of birth in terms of milliseconds since 1/1/1970.
  */
 public class JSON_Employee extends JDX_JSONObject {
 
