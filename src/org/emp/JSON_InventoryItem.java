f309c2e41e45add02a564d145283e497195cc063
NikhilVinay04 <130574166+NikhilVinay04@users.noreply.github.com>
2024-07-04 12:49:52 +0530

    Update JSON_InventoryItem.java

--- src/org/emp/JSON_InventoryItem.java
+++ src/org/emp/JSON_InventoryItem.java
@@ -5,6 +5,17 @@ import org.json.JSONObject;
 
 import com.softwaretree.jdx.JDX_JSONObject;
 
+/**
+ * A shell (container) class defining a domain model object class for Inventory objects
+ * based on the class JSONObject.  This class needs to define just two constructors.
+ * Most of the processing is handled by the superclass JDX_JSONObject.
+ * Description of the Inventory type object : 
+ *    itemID-String primary key.
+ *    itemName- Name of the item.
+ *    quantity- Number of items in Inventory for a particular itemID.
+ *    dob- long value to describe date in terms of milliseconds since 1/1/1970.
+ */
+
 public class JSON_InventoryItem extends JDX_JSONObject {
     public JSON_InventoryItem() {
         super();
